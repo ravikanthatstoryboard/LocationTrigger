d0cf8a02389700160475009fe7ca22b2e4f9a53f
Ravikanth <ravikanthcse2011@gmail.com>
2018-03-28 17:17:54 +0530

    first commit

---
+++ LocationTrigger.framework/Headers/LocationTrigger.h
@@ -0,0 +1,19 @@
+//
+//  LocationTrigger.h
+//  LocationTrigger
+//
+//  Created by Ravikanth on 28/03/18.
+//  Copyright © 2018 StoryBoard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocationTrigger.
+FOUNDATION_EXPORT double LocationTriggerVersionNumber;
+
+//! Project version string for LocationTrigger.
+FOUNDATION_EXPORT const unsigned char LocationTriggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocationTrigger/PublicHeader.h>
+
+
---
+++ LocationTrigger/LocationTrigger.swift
@@ -0,0 +1,15 @@
+//
+//  LocationTrigger.swift
+//  LocationTrigger
+//
+//  Created by Ravikanth on 28/03/18.
+//  Copyright © 2018 StoryBoard. All rights reserved.
+//
+
+import Foundation
+
+public class LocationTrigger {
+    public class func logToConsole(msg: String) {
+        print(msg);
+    }
+}
